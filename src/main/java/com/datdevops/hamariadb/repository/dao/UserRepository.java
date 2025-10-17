db63da9d9a0446ee688a7ee05881c908e99d5598
datdev <Dotiendat1751998@gmail.com>
2025-10-17 14:57:39 +0700

    update user respository

--- src/main/java/com/datdevops/hamariadb/repository/dao/UserRepository.java
+++ src/main/java/com/datdevops/hamariadb/repository/dao/UserRepository.java
@@ -1,13 +1,15 @@
 package com.datdevops.hamariadb.repository.dao;
 
 
-import com.datdevops.hamariadb.entity.User;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import com.datdevops.hamariadb.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
@@ -18,4 +20,8 @@ public interface UserRepository extends JpaRepository<User, String> {
 
     @Query("SELECT u FROM User u LEFT JOIN FETCH u.userRoles WHERE u.username = :username")
     Optional<User> findByUsernameWithRoles(@Param("username") String username);
+  
+    @Modifying
+    @Query("UPDATE User u SET u.failedLoginAttempts = 0 WHERE u.username = :username")
+    void resetFailedLoginAttempts(@Param("username") String username);
 }
