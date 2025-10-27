eb10b5cf8a4e5d17db062dcbf99c2d81e45823c1
datdev <Dotiendat1751998@gmail.com>
2025-10-27 14:44:46 +0700

    update notification

--- src/main/java/com/datdevops/hamariadb/service/notification/TelegramService.java
+++ src/main/java/com/datdevops/hamariadb/service/notification/TelegramService.java
@@ -70,8 +70,8 @@ public class TelegramService {
 
         String emoji = amount.compareTo(BigDecimal.ZERO) > 0 ? "💹" : "📤";
         String message = String.format(
-                "%s Tài khoản <b>%s</b>\nTài Khoản nhận <b>%s</b> \nTên người nhận <b>%s</b> \nSố tiền:<b>%s</b>\nPhí: <b> -%s</b>\nSố dư: <b>%s</b>\nNội dung: %s",
-                emoji, accountNumber, toAccountNumber, toAccountName, amountStr, feeStr, balanceStr, description
+                "%s Tài khoản <b>%s</b>\nTên Khoản nhận <b>%s</b> \n Tài khoản nhận <b>%s</b> \nSố tiền:<b>%s</b>\nPhí: <b> -%s</b>\nSố dư: <b>%s</b>\nNội dung: %s",
+                emoji, accountNumber, toAccountName, toAccountNumber, amountStr, feeStr, balanceStr, description
         );
 
         sendMessage(telegramConfig.getAdminChatId(), message);
